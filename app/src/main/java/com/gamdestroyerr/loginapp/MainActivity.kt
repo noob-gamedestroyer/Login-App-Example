55944f2fc7441c494da37176fb392a1788e6aff9
noob-gamedestroyer <60071765+noob-gamedestroyer@users.noreply.github.com>
2020-08-10 18:30:26 +0530

    First commit, Basic login ui framework is 80% done

---
+++ app/src/main/java/com/gamdestroyerr/loginapp/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.gamdestroyerr.loginapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_dialog_container.view.*
+import kotlinx.android.synthetic.main.register_bottom_sheet.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mBottomSheetBehavior = BottomSheetBehavior.from(registerBottomSheet)
+
+        passwordTxtInputLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+        setPasswordTxtInput.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+        confirmPasswordTxtInput.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+        bottomSheetContainer.elevation = 6f
+
+        val factory = LayoutInflater.from(this)
+        val view1 :View = factory.inflate(R.layout.custom_dialog_container, null)
+
+        registerBtn.setOnClickListener {
+
+            mBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            mBottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback(){
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    cancelBtn.rotation = (slideOffset * 135)
+                }
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+                    signUpBtn.setOnClickListener {
+                        mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                        showDialog(view1)
+                    }
+
+                }
+            })
+        }
+
+        cancelBtn.setOnClickListener {
+            if (mBottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+                mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun showDialog(view1: View) {
+
+        val dialog = MaterialAlertDialogBuilder(this)
+            .setView(view1)
+            .setBackground(getDrawable(R.drawable.rounded_dialog))
+            .setCancelable(false)
+            .show()
+
+        view1.dialogBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+}
