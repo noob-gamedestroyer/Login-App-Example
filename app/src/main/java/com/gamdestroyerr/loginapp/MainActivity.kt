c5f0b5afeff520b16f564a62fc7c1c19e89f1013
noob-gamedestroyer <60071765+noob-gamedestroyer@users.noreply.github.com>
2020-08-11 14:31:12 +0530

    implemented new password and confirm password equality check: displays error message if mismatch

--- app/src/main/java/com/gamdestroyerr/loginapp/MainActivity.kt
+++ app/src/main/java/com/gamdestroyerr/loginapp/MainActivity.kt
@@ -4,21 +4,40 @@ import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.custom_dialog_container.view.*
 import kotlinx.android.synthetic.main.register_bottom_sheet.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        auth = FirebaseAuth.getInstance()
+        var newPass:String?
+        var conPass:String?
+
+
         val mBottomSheetBehavior = BottomSheetBehavior.from(registerBottomSheet)
 
         passwordTxtInputLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
@@ -41,27 +60,34 @@ class MainActivity : AppCompatActivity() {
                     cancelBtn.rotation = (slideOffset * 135)
                 }
 
-                @SuppressLint("UseCompatLoadingForDrawables")
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
-
-                    signUpBtn.setOnClickListener {
-                        signUpBtn.visibility = View.INVISIBLE
-
-                        val dialog = MaterialAlertDialogBuilder(this@MainActivity)
-                            .setView(view1)
-                            .setBackground(getDrawable(R.drawable.rounded_dialog))
-                            .setCancelable(false)
-                            .show()
-
-                        view1.dialogBtn.setOnClickListener {
-                            dialog.dismiss()
-                            mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-                            signUpBtn.visibility = View.VISIBLE
-                        }
-                    }
-
                 }
+
+
             })
+
+        }
+
+        signUpBtn.setOnClickListener {
+            newPass = setPasswordTxtInput.editText?.text.toString()
+            conPass = confirmPasswordTxtInput.editText?.text.toString()
+
+            if (newPass == conPass) {
+                signUpBtn.visibility = View.INVISIBLE
+                val dialog = MaterialAlertDialogBuilder(this@MainActivity)
+                    .setView(view1)
+                    .setBackground(getDrawable(R.drawable.rounded_dialog))
+                    .setCancelable(false)
+                    .show()
+
+                view1.dialogBtn.setOnClickListener {
+                    dialog.dismiss()
+                    mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                    signUpBtn.visibility = View.VISIBLE
+                }
+            } else {
+                passwordMatchConfirmation.visibility = View.VISIBLE
+            }
         }
 
         cancelBtn.setOnClickListener {
@@ -71,6 +97,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+
+    private fun registerUser() {
+
+        val email = emailTxtRegister.editText.toString().trim()
+        val password = setPasswordTxtInput.editText.toString()
+
+
+    }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
 
